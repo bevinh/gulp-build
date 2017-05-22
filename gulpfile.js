f321dd009bf163fcfdec3f4cc990a449c8dd9f99
bevinh <bevin.hernandez@gmail.com>
2017-05-22 12:04:44 -0400

    completed css min

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
     csso = require('gulp-csso'),
     sourcemaps = require('gulp-sourcemaps'),
     del = require('del'),
@@ -8,6 +9,7 @@ var gulp = require('gulp'),
 
 var options = {
     scripts: ['src/js/global.js', 'src/js/circle/circle.js', 'src/js/circle/autogrow.js'],
+    cssfiles: ['src/sass/**/*.scss', 'sass/circle/**/*.sass'],
     src: 'src'
 };
 
@@ -27,18 +29,23 @@ gulp.task('scripts', ['clean'], function() {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/js'));
 });
+gulp.task('sass', ['clean'], function(){
+    return gulp.src(options.cssfiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('tmp/css'));
+});
 
-gulp.task('styles', ['clean'], function(){
+gulp.task('styles', ['clean', 'sass'], function(){
     //Minify and copy all css with sourcemaps
-    return gulp.src(options.cssFiles)
-        .pipe(csso({
-            restructure: true,
-            sourceMap: true,
-            debug: true
-        }))
+    return gulp.src('tmp/css/global.css')
+        .pipe(sourcemaps.init())
+        .pipe(csso())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/styles/'));
 })
 
 
 gulp.task('clean', function() {
-    return del(['dist']);
+    return del(['dist', 'tmp']);
+
 });
